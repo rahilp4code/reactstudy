743669d488afaa23e77bc84d0eefc3cfa0e2d2cb
rahilp4code <pathanrahil2244@gmail.com>
2025-10-04 23:45:49 +0530

    started quiz

--- 06-quiz/src/App.js
+++ 06-quiz/src/App.js
@@ -1,7 +1,29 @@
-import "./styles.css";
+import Header from "./component/Header";
+import "./index.css";
 // import Counter from "./component/counter";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
+
+const initialVal = {
+  questions: [],
+
+  // loading, active, error, finished, ready
+  status: "loading",
+};
+
+function reducer() {}
 
 export default function App() {
-  return <div className="App">hello</div>;
+  const [state, dispatch] = useReducer(reducer, initialVal);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err.message));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
